5d8e5cd7fa3e491ddd96b1ae70478faad68716a8
Andrea Parodi <andrea@parro.it>
2017-03-31 08:53:38 +0200

    improved test

--- test.js
+++ test.js
@@ -16,8 +16,9 @@ function asyncIterableFrom(array) {
 test('command with one argument', async t => { // eslint-disable-line ava/no-async-fn-without-await
 	const source = asyncIterableFrom([1, 2, 3, 42, 43]);
 	const result = [];
-	for await (const value of map(i => i, source)) { // eslint-disable-line semi
+	const doubleFn = n => n * 2;
+	for await (const value of map(doubleFn, source)) { // eslint-disable-line semi
 		result.push(value);
 	}
-	t.deepEqual(result, [1, 2, 3, 42, 43]);
+	t.deepEqual(result, [2, 4, 6, 84, 86]);
 });
