d63b3fb4d40ffed04a416635b4880970ecdb1f2d
Andrea Parodi <andrea@parro.it>
2017-03-31 09:27:14 +0200

    refactor

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 import curry from 'curry';
 
-async function * map(transform, source) {
-	const asyncIterator = Symbol.asyncIterator || Symbol('asyncIterator');
-	Symbol.asyncIterator = asyncIterator;
+const asyncIterator = Symbol.asyncIterator || Symbol('asyncIterator');
+Symbol.asyncIterator = asyncIterator;
 
+function isAsyncIterable(source) {
+	const isObject = source !== null && typeof source === 'object';
+	if (!isObject) {
+		return false;
+	}
+
+	return typeof source[asyncIterator] === 'function' ||
+		typeof source[Symbol.iterator] === 'function';
+}
+
+async function * map(transform, source) {
 	if (typeof transform !== 'function') {
 		throw new TypeError('First argument must be a function.');
 	}
 
-	if (source === null ||
-		typeof source !== 'object' ||
-
-		(typeof source[asyncIterator] !== 'function' &&
-		typeof source[Symbol.iterator] !== 'function')) {
+	if (!isAsyncIterable(source)) {
 		throw new TypeError('Second argument must be an asyncIterable or iterable.');
 	}
 	for await (const item of source) { // eslint-disable-line semi
