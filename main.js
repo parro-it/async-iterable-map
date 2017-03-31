080ded9895e7757d84bcc970eb0abfbb43001f09
Andrea Parodi <andrea@parro.it>
2017-03-31 09:23:53 +0200

    check arguments

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 import curry from 'curry';
 
-async function * map(func, source) {
+async function * map(transform, source) {
+	const asyncIterator = Symbol.asyncIterator || Symbol('asyncIterator');
+	Symbol.asyncIterator = asyncIterator;
+
+	if (typeof transform !== 'function') {
+		throw new TypeError('First argument must be a function.');
+	}
+
+	if (source === null ||
+		typeof source !== 'object' ||
+
+		(typeof source[asyncIterator] !== 'function' &&
+		typeof source[Symbol.iterator] !== 'function')) {
+		throw new TypeError('Second argument must be an asyncIterable or iterable.');
+	}
 	for await (const item of source) { // eslint-disable-line semi
-		yield func(await item);
+		yield transform(await item);
 	}
 }
 
--- test.js
+++ test.js
@@ -24,20 +24,39 @@ async function concat(asyncIterable) {
 	return result;
 }
 
-test('Apply the function to all items', async t => { // eslint-disable-line ava/no-async-fn-without-await
+test('Apply the function to all items', async t => {
 	const source = asyncIterableFrom([1, 2, 3, 42, 43]);
 	const result = await concat(map(doubleFn, source));
 	t.deepEqual(result, [2, 4, 6, 84, 86]);
 });
 
-test('Resolve promise items', async t => { // eslint-disable-line ava/no-async-fn-without-await
+test('Resolve promise items', async t => {
 	const source = asyncIterableFrom([1, Promise.resolve(2)]);
 	const result = await concat(map(doubleFn, source));
 	t.deepEqual(result, [2, 4]);
 });
 
-test('Await transformer result', async t => { // eslint-disable-line ava/no-async-fn-without-await
+test('Await transformer result', async t => {
 	const source = asyncIterableFrom([1, Promise.resolve(2)]);
 	const result = await concat(map(promise, source));
 	t.deepEqual(result, [1, 2]);
+});
+
+test('is Curried', async t => {
+	const source = asyncIterableFrom([1, Promise.resolve(2)]);
+	const doubleThem = map(doubleFn);
+	const result = await concat(doubleThem(source));
+	t.deepEqual(result, [2, 4]);
+});
+
+test('throw if first argument is not a function', async t => {
+	const err = await t.throws(concat(map(42, null)));
+	t.is(err.message, 'First argument must be a function.');
+	t.true(err instanceof TypeError);
+});
+
+test('throw if second argument is not an asyncIterable or iterable', async t => {
+	const err = await t.throws(concat(map(doubleFn, 42)));
+	t.is(err.message, 'Second argument must be an asyncIterable or iterable.');
+	t.true(err instanceof TypeError);
 });
